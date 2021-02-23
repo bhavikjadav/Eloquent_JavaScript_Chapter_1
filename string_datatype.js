725acb0ee7b7b68a903da2b2dbf8ff308d34d748
Bhavik Jadav <bhavik.jadav89@gmail.com>
2021-02-23 12:34:37 +0530

    Add files via upload

---
+++ string_datatype.js
@@ -0,0 +1,47 @@
+console.log(`Down the sea`);
+console.log("Lie on the Ocean");
+console.log('Float on the ocean');
+
+// There are 3 type a string predsenataion methods in JavaScipt.
+
+console.log("This is the first line\nAnd this is the second");
+
+console.log("The new line character is written like \"\\n\".");
+
+console.log("con" + "cat" + "e" + "nate");
+
+console.log(`half of 100 is ${100/2}.`);
+
+console.log(typeof 4.5);
+console.log(typeof "x");
+
+console.log(-(10-2));
+
+console.log(3>2);
+console.log(3<2);
+
+console.log("Bhavik" < "Jadav");
+
+console.log("Bhavik" != "Jadav");
+console.log("Apple" == "Orange");
+
+// There is only one value in JavaScript that is not equal to itself, and that is NaN ("Not a Number").
+console.log(NaN == NaN);
+
+console.log(true && false); // false
+console.log(true && true); // true
+
+console.log(true || true); // true
+console.log(false || false); // false
+console.log(true || false); // true
+
+console.log(1 + 1 == 2 && 10 * 10 > 50); // true
+
+console.log(true ? 1:2); // 1.
+console.log(false ? 1:2); // 2.
+
+console.log(8 * null); //0.
+console.log("5" - 1); //4.
+console.log("5" + 1); //51.
+console.log("five" * 2); //NaN
+console.log(false == 0); //true      
